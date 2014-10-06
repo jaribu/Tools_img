8dba4777545fea004db02a83049377e0ca1f8b9c
jaribu <jaribu59@gmail.com>
2014-10-06 15:18:02 +0200

    Fix Gruntfile + package.json & amélioration du readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
 grunt.initConfig({
   imagemin: {                          // Task
-    dynamic: {                         // Another target
+    dynamic: {                         // Another target  
       files: [{
         expand: true,                  // Enable dynamic expansion
         cwd: 'src/',                   // Src matches are relative to this path
@@ -10,19 +10,9 @@ grunt.initConfig({
         dest: 'dist/'                  // Destination path prefix
       }]
     }
-  },
-  imageoptim: {
-  myTask: {
-    src: ['src/']
   }
-}
 });
 
-
 grunt.loadNpmTasks('grunt-contrib-imagemin');
-grunt.loadNpmTasks('grunt-imageoptim');
-
-
 grunt.registerTask('default', ['imagemin']);
-grunt.registerTask('optim', ['imageoptim']);
 };
--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Tools_img
 =========
 
 Tools pour optimiser les images 
+
+1/ Lancer npm install
+
+2/ Ajouter les images dans le dossier src
+
+3/ Lancer Grunt
+
+4/ Récupérer les images dans le dossier dist 
+
+Reste à faire une tâche pour recompresser les images
